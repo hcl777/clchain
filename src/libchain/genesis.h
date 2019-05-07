aec787d4b377cb52b026669a74372219150f9514
long <he_cl777@163.com>
2019-05-07 13:21:12 +0800

    long

--- src/libchain/genesis.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "cltype.h"
-#include "block.h"
-
-
-int genesis_load_json(const std::string& path, block_t& b);
-int genesis_chain_setting(const std::string& jsonstr, chain_setting_t& set);
-
--- src/libchain/trarun.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-int tra_main(int argc, char** argv);
-
-
-
